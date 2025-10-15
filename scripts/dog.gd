dafa9e04e90a540dba76a83d946cb9b2239f9c38
Jeppe Lindberg <jepp3441@gmail.com>
2025-10-15 20:30:13 +0200

    progress

--- scripts/dog.gd
+++ scripts/dog.gd
@@ -9,6 +9,7 @@ var direction_to_player = Vector2.ZERO
 @onready var player = get_node('/root/main/player')
 @onready var shield = get_node('shield')
 
+@export var sprite: AnimatedSprite2D
 @export var movement_speed : float = 300.0
 
 @export_flags_2d_physics var player_layer: int
@@ -18,6 +19,15 @@ func _physics_process(_delta: float) -> void:
 	_update_state()
 
 	direction_to_player = (player.global_position - global_position).normalized()
+
+	if state == 'idle' or state == 'move_toward_player':
+		sprite.play('idle')
+	if state == 'sit':
+		sprite.play('sit')
+	if direction_to_player.x < 0:
+		sprite.scale.x = -1
+	else:
+		sprite.scale.x = 1
 
 	if (state == 'idle') or (state =='sit'):
 		linear_velocity = Vector2.ZERO
